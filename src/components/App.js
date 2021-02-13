09823dec3a3207f27a6ef3229143eecd757f7a15
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-13 09:46:52 +0000

    added App.js with classes

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+	return (
+		<div className="parent">
+			<h1 className="title">Hello World!!!</h1>
+			<h2 className="child">How are you?</h2>
+			<p id="parap">I'm fine</p>
+		</div>
+	);
+};
+
+export default App;
